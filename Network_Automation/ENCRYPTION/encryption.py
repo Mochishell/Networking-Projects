722749ae8ae3caf337512e79254d15f50fca5d51
kemi <brandonduongneon@gmail.com>
2021-07-20 23:31:06 -0700

    added simplecrypt import statement

--- Network_Automation/ENCRYPTION/encryption.py
+++ Network_Automation/ENCRYPTION/encryption.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+from simplecrypt import encrypt, decrypt
 from netmiko import ConnectHandler
 from config_modules import create_vlans, config_general, read_devices
 import getpass
