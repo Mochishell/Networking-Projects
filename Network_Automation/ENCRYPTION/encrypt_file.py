0d6b3af95768a1c172336af9068c672f8444ed18
kemi <brandonduongneon@gmail.com>
2021-07-20 23:06:22 -0700

    added encryption and decryption

--- Network_Automation/ENCRYPTION/encrypt_file.py
+++ Network_Automation/ENCRYPTION/encrypt_file.py
@@ -12,6 +12,16 @@ user_key = input('Input your encryption key: ') or 'cisco'
 
 #now, need to encrypt the file
 with open(user_input, 'r') as file:
-    device_creds_reader = csv.reader(file)
-    for device in device_creds_reader:
-        print(device)
+    device_creds_reader = csv.reader(file, delimiter = ',')
+    device_creds_list = [device for device in device_creds_reader]
+
+encrypted_file_name = '{}-encrypted'.format(user_input)
+decrypted_file_name = '{}-decrypted'.format(user_input)
+
+with open(encrypted_file_name, 'wb') as file:
+    file.write(encrypt(user_key, json.dumps(device_creds_list)))
+
+with open(encrypted_file_name, 'rb') as file:
+    decrypted_file = json.loads( decrypt( user_key, file.read()))
+    print(decrypted_file)
+
