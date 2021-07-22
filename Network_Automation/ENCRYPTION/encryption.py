466c7c47e0d8b327b0e437650693c5451e7f9922
kemi <brandonduongneon@gmail.com>
2021-07-21 19:47:02 -0700

    modified devices.txt to have correct IOS, changed encryption.py to start connecting to devices

--- Network_Automation/ENCRYPTION/encryption.py
+++ Network_Automation/ENCRYPTION/encryption.py
@@ -9,8 +9,14 @@ import json
 devices_dict = config_modules.read_devices('devices.txt')
 
 #should print a dictionary of dictionaries
-print(config_modules.get_device_creds('password.txt-encrypted', 'cisco'))
+device_creds = config_modules.get_device_creds('password.txt-encrypted', 'cisco')
 
+IP = ['192.168.0.11', '192.168.0.12', '192.168.0.13']
+session = ConnectHandler( device_type=devices_dict['192.168.0.11']['type'], ip=devices_dict['192.168.0.11']['ipaddr'],
+                        username=device_creds['192.168.0.11']['username'],
+                        password=device_creds['192.168.0.11']['password'] )
+
+print(session.send_command('sh ip int brief'))
 
 
 
