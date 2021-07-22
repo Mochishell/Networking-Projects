112921f4fd30caa67366a8b6776682dc95ddebbb
kemi <brandonduongneon@gmail.com>
2021-07-21 19:57:47 -0700

    adding some test print statements to verify device

--- Network_Automation/ENCRYPTION/encryption.py
+++ Network_Automation/ENCRYPTION/encryption.py
@@ -12,11 +12,19 @@ devices_dict = config_modules.read_devices('devices.txt')
 device_creds = config_modules.get_device_creds('password.txt-encrypted', 'cisco')
 
 IP = ['192.168.0.11', '192.168.0.12', '192.168.0.13']
-session = ConnectHandler( device_type=devices_dict['192.168.0.11']['type'], ip=devices_dict['192.168.0.11']['ipaddr'],
-                        username=device_creds['192.168.0.11']['username'],
-                        password=device_creds['192.168.0.11']['password'] )
 
-print(session.send_command('sh ip int brief'))
+for device_ip in IP:
+
+    session = ConnectHandler( device_type=devices_dict[device_ip]['type'], ip=devices_dict[device_ip]['ipaddr'],
+                            username=device_creds[device_ip]['username'],
+                            password=device_creds[device_ip]['password'] )
+
+    print(session.send_command('sh ip int brief'))
+
+    if devices_dict[device_ip]['type'] == 'cisco_ios':
+        print ('this is a cisco router')
+
+
 
 
 
