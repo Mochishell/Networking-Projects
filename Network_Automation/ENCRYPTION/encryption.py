77f813ab739cc8f4ccc544b1f7ae25837d1dfca9
kemi <brandonduongneon@gmail.com>
2021-07-21 20:31:51 -0700

    takes user input for devices, password files and encryption key

--- Network_Automation/ENCRYPTION/encryption.py
+++ Network_Automation/ENCRYPTION/encryption.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#assumes only cisco ios devices
 from simplecrypt import encrypt, decrypt
 from netmiko import ConnectHandler
 import config_modules
@@ -7,11 +8,15 @@ import getpass
 import json
 
 #creating dictionary of devices, where each device is also a dictionary
-devices_dict = config_modules.read_devices('devices.txt')
+user_input = input("Please input a file containing devices: ")
+devices_dict = config_modules.read_devices(user_input)
 
 #creating a dictionary of device, where each device is also a dictionary
 #separates passwords
-device_creds = config_modules.get_device_creds('password.txt-encrypted', 'cisco')
+user_password_input = input("Please input your encrypted password file: ")
+user_encryption_key = input("Please input your encryption/decryption key: ")
+
+device_creds = config_modules.get_device_creds(user_password_input, user_encryption_key)
 
 for device_ip in devices_dict.keys():
 
