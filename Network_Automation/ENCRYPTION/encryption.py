4e3e1bb21b8437c66793570548f79b33babd033b
kemi <brandonduongneon@gmail.com>
2021-07-21 20:27:04 -0700

    writing print statements for clarity, deleted extra list

--- Network_Automation/ENCRYPTION/encryption.py
+++ Network_Automation/ENCRYPTION/encryption.py
@@ -13,20 +13,20 @@ devices_dict = config_modules.read_devices('devices.txt')
 #separates passwords
 device_creds = config_modules.get_device_creds('password.txt-encrypted', 'cisco')
 
-IP = ['192.168.0.11', '192.168.0.12', '192.168.0.13']
-
-for device_ip in IP:
+for device_ip in devices_dict.keys():
 
     session = ConnectHandler( device_type=devices_dict[device_ip]['type'], ip=devices_dict[device_ip]['ipaddr'],
                             username=device_creds[device_ip]['username'],
                             password=device_creds[device_ip]['password'] )
 
-    print(session.send_command('sh ip int brief'))
+
 
     if devices_dict[device_ip]['type'] == 'cisco_ios':
         print ('this is a cisco router')
 
+    #writing to file the running config of the current switch, file name corresponds to switch
     with open( "{}".format(devices_dict[device_ip]['name']) + '.cfg', 'w') as f:
+        print('Writing running config to file {}'.format(devices_dict[device_ip]['name']) + '.cfg')
         f.write(session.send_command('sh run'))
 
 
