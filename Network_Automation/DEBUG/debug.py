55b55b38bdb6abde639f71a2b240944d35213c3f
kemi <brandonduongneon@gmail.com>
2021-07-27 11:42:00 -0700

    testing connection to single device

--- Network_Automation/DEBUG/debug.py
+++ Network_Automation/DEBUG/debug.py
@@ -7,4 +7,9 @@ import json
 
 devices_dict = config_modules.read_devices('devices.txt')
 devices_creds = config_modules.get_device_creds_unencrypted('password.txt')
-print(devices_creds)
+
+
+session = ConnectHandler( device_type=devices_dict['192.168.0.11']['type'], ip=devices_dict['192.168.0.11']['ipaddr'],
+                                username=devices_creds['192.168.0.11']['username'],
+                                password=devices_creds['192.168.0.11']['password'] )
+
