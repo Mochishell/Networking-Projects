7fde39b4f26ec4d9923a2e0c46aec78812ee3d2e
kemi <brandonduongneon@gmail.com>
2021-07-27 12:10:31 -0700

    writing sh arp command to file for single switch

--- Network_Automation/DEBUG/debug.py
+++ Network_Automation/DEBUG/debug.py
@@ -3,7 +3,7 @@ import config_modules
 import getpass
 import json
 
-#start small
+#start small, active debug probably won't work, lets try to just do show commands and save them to files
 
 devices_dict = config_modules.read_devices('devices.txt')
 devices_creds = config_modules.get_device_creds_unencrypted('password.txt')
@@ -13,3 +13,8 @@ session = ConnectHandler( device_type=devices_dict['192.168.0.11']['type'], ip=d
                                 username=devices_creds['192.168.0.11']['username'],
                                 password=devices_creds['192.168.0.11']['password'] )
 
+print(session.send_command('sh arp'))
+
+#writing arp table output to file
+with open('{}_arp.txt'.format(devices_dict['192.168.0.11']['name']), 'w') as f:
+    f.write(session.send_command('sh arp'))
