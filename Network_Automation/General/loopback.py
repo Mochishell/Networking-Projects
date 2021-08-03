e6baf90df156292ce5fbbe3b8692d4f1a352f78a
kemi <brandonduongneon@gmail.com>
2021-08-03 01:06:16 +0000

    fixed configuration of loopback interface

--- Network_Automation/General/loopback.py
+++ Network_Automation/General/loopback.py
@@ -10,21 +10,21 @@ logger = logging.getLogger("netmiko")
 
 devices_dict = config_modules.read_devices('devices.txt')
 devices_creds = config_modules.get_device_creds_unencrypted('password.txt')
-devices_loopbacks = []
 
 #reading in loopbacks from csv
 with open('loopback.txt') as f:
     reader = csv.reader(f)
-    for row in reader:
-        devices_loopbacks.append(row)
+    devices_loopbacks_dict = {row[0]: (row[1], row[2]) for row in reader}
 
-print(devices_loopbacks)
+
 #keys are ip addresses
 for device in devices_dict:
 
     session = ConnectHandler( device_type=devices_dict[device]['type'], ip=devices_dict[device]['ipaddr'] ,
                                 username=devices_creds[device]['username'], password=devices_creds[device]['password'])
-
     
+    #configuring loopback for device
+    config_modules.config_loopback(session, devices_loopbacks_dict[device][0], devices_loopbacks_dict[device][1])    
+
 #TODO:
 
