12048de1a548245f0b332afd43c8b2cac2d85ea4
kemi <brandonduongneon@gmail.com>
2021-08-20 17:38:39 -0700

    cleaned up directory, put learning materials into learning directory

--- Network_Automation/General/debug_multithread.py
+++ Network_Automation/General/debug_multithread.py
@@ -24,13 +24,13 @@ def main():
         session = ConnectHandler( device_type=devices_dict[device]['type'], ip=devices_dict[device]['ipaddr'] ,
                                     username=devices_creds[device]['username'], password=devices_creds[device]['password'])
         config_param_list.append( session )
-        #config_modules.debug_arp_cache(session)
-        #config_modules.debug_running_config(session)
-        #config_threads_list.append(threading.Thread(target=config_modules.debug_arp_cache, args=session))
 
+    #limiting threads
     threads = ThreadPool( num_threads )
+
     arp_results = threads.map(config_modules.debug_arp_cache, config_param_list)
     running_config_results = threads.map(config_modules.debug_running_config, config_param_list)
+
     threads.close()
     threads.join()
 
